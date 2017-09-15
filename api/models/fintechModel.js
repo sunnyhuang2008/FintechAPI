20a315237c1d196db534ee7ae837ed561e958fea
Sunny Huang <sunny-20080101@hotmail.com>
2017-09-14 22:14:11 -0400

    Added new models

--- api/models/fintechModel.js
+++ api/models/fintechModel.js
@@ -36,14 +36,44 @@ optimization.pre('save', function(next){
 
 //TBD on alterOptimization Design 
 var alterOptimization = new Schema({
+	// Entry ID for each optimization object entry
 	optimizationID : String, 
 	holdings : [
-		hoiding : {
+		holding : {
 			ticker : String, 
 			weight : Number,
 			sector : String 
 		}
+	],
+	time : {
+		created_at : Date
+	}
+});
+
+alterOptimization.pre('save', function(next){
+	var currentTime = new Date;
+	this.time = currentTime;
+});
+
+var portfolio = new Schema({
+	name : String,
+	marketValue : [
+		valueEntry : {
+			value : Number,
+			currency : String, 
+			time : Date
+		}
+	],
+	historicalOptimization : [
+		optimizationID : String
 	]
+// More portfolio meta metrics to be added 
+});
+
+portfolio.pre('save', function(next){
+	var currentTime = new Date;
+	this.marketValue.valueEntry.time = currentTime;
+	next();
 });
 
 var ticker = new Schema({
@@ -52,7 +82,11 @@ var ticker = new Schema({
 });
 
 var optimization = mongoose.model('optimization', optimization);
+var portfolio = mongoose.model('assetValue',portfolio);
 var alterOptimization = mongoose.model('alterOptimization', alterOptimization);
+var ticker = mongoose.model('ticker', ticker);
 
 module.exports = optimization;
+module.exports = portfolio;
 module.exports = alterOptimization;
+module.exports = ticker;
