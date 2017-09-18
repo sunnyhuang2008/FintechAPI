2da730e9a55bdadc9c0f775fa0e5fb356200d090
Sunny Huang <sunny-20080101@hotmail.com>
2017-09-17 20:28:38 -0400

    Changed controller to accommodate change in market value schema

--- api/controllers/fintechController.js
+++ api/controllers/fintechController.js
@@ -16,25 +16,18 @@ exports.list_ticker_price = function(req, res) {
   }).end();
 };
 
-exports.save_portfolio = function(req, res){
+//Turn this into an internal function that takes in the value, currency and the portfolio name 
+exports.save_portfolio_value = function(req, res){
 
-	var portfolio = mongoose.model('portfolio', dataModel.portfolio);
-	var testPortfolio = new portfolio({
+	var portfolio_value = mongoose.model('portfolio_value', dataModel.portfolio_value);
+	var testPortfolio_value = new portfolio_value({
 		name : "testPortfolio",
-		marketValue : [],
-		historicalOptimization :[]
+		value : 34783.46,
+		currency : "USD",
+		time : null
 	});
 
-	var currentTime = new Date;
-	var marketValue ={
-		"value" : 387987.98,
-		"currency" : "USD",
-		"time" : currentTime
-	}
-
-	testPortfolio.marketValue.push(marketValue);
-
-	testPortfolio.save(function(err){
+	testPortfolio_value.save(function(err){
 		if(err) throw err;
 		res.status(200).send('Succesfully created an entry in portfolio collection!');
 	});
