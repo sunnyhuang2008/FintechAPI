2a86e3b21722f92b01d845668c2ca1fac716cd49
Sunny Huang <sunny-20080101@hotmail.com>
2017-09-17 20:29:55 -0400

    Change to accommodate model change

--- api/routes/fintechRoutes.js
+++ api/routes/fintechRoutes.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
     .get(fintech.list_ticker_price);
 
   app.route('/testSavePortfolio')
-  .get(fintech.save_portfolio);
+  .get(fintech.save_portfolio_value);
 
  
 };
