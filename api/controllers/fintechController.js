4a444e41bc4b9f8be08c6cbbc2ea463cfad41b07
Jason <Stewie@Jasons-MacBook-Air.local>
2017-10-08 01:48:28 -0700

    issue 6: Pull realtime intraday stock data from Alpha Vantage API

--- api/controllers/fintechController.js
+++ api/controllers/fintechController.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose'),
   dataModel = require('../models/fintechModel.js'),
   config = require('../config.js');
 
-mongoose.connect(config.db.uri);
+//mongoose.connect(config.db.uri);
 
 exports.list_ticker_price = function(req, res) {
  request('https://www.quandl.com/api/v3/datasets/WIKI/WMT.json?start_date=1985-05-01&end_date=2010-07-01&order=asc&column_index=4&collapse=daily&transformation=rdiff',
@@ -19,21 +19,29 @@ exports.list_ticker_price = function(req, res) {
 //Turn this into an internal function that takes in the value, currency and the portfolio name 
 exports.save_portfolio_value = function(req, res){
 
-	var portfolio_value = mongoose.model('portfolio_value', dataModel.portfolio_value);
-	var testPortfolio_value = new portfolio_value({
-		name : "testPortfolio",
-		value : 34783.46,
-		currency : "USD",
-		time : null
-	});
+  var portfolio_value = mongoose.model('portfolio_value', dataModel.portfolio_value);
+  var testPortfolio_value = new portfolio_value({
+    name : "testPortfolio",
+    value : 34783.46,
+    currency : "USD",
+    time : null
+ });
 
-	testPortfolio_value.save(function(err){
-		if(err) throw err;
-		res.status(200).send('Succesfully created an entry in portfolio collection!');
-	});
+ testPortfolio_value.save(function(err){
+    if(err) throw err;
+    res.status(200).send('Succesfully created an entry in portfolio collection!');
+ });
 };
 
+exports.get_realtime_stock_data = function(req, res) {
+  var ticker = req.params.ticker;
 
-
-
-
+  request('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol='+ ticker +'&apikey=04G9FLWCWQG6NRJO', function(err, response, body){
+    if(!err && response.statusCode == 200){
+      var jsonRes = JSON.parse(body);
+      var timeSeries = jsonRes[Object.keys(jsonRes)[1]];
+      var latest = timeSeries[Object.keys(timeSeries)[0]];
+      res.json(latest[Object.keys(latest)[3]]);
+    }
+  }).end();
+};
--- api/routes/fintechRoutes.js
+++ api/routes/fintechRoutes.js
@@ -9,5 +9,5 @@ module.exports = function(app) {
   app.route('/testSavePortfolio')
   .get(fintech.save_portfolio_value);
 
- 
+  app.get('/getRealtimeStockData/:ticker',fintech.get_realtime_stock_data);
 };
